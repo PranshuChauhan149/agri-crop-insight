c3712548c121c7652faba0a6465e891462b2fabb
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-12-04 00:39:20 +0530

    frontend and backend setup compeleted

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-// this is ServiceWorkerRegistrationfvfvx
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("server is running...");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on this PORT: ${PORT} `);
+});
